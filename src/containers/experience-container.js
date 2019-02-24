142933f629bd1eb5389ceb04fea51adda9a22642
Aleksi Vuorela <aleksii.vuorela@gmail.com>
2019-02-24 20:05:12 +0200

    fix

--- src/containers/experience-container.js
+++ src/containers/experience-container.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import Experience from '../components/Experience'
+import Experience from '../components/experience'
 
 const ExperienceContainer = () => {
   return (
